cf598ecf233a74b2e131b2f34a5f8b4c24215a82
Paul Gross <p.gross6408@gmail.com>
2024-10-02 14:03:54 +0200

    fixing from SPA to MPA

--- js/load_layout.js
+++ js/load_layout.js
@@ -1,6 +1,6 @@
 async function loadHeader() {
   try {
-    const response = await fetch("/patials/header.html");
+    const response = await fetch("../patials/header.html");
     if (!response.ok) {
         console.error("keine Datai namen header.html gefunden")
     }
@@ -14,14 +14,36 @@ loadHeader();
 
 async function loadSidebar() {
   try {
-    const response = await fetch("/patials/sidebar.html");
+    const response = await fetch("../patials/sidebar.html");
     if (!response.ok) {
-        console.error("keine Datai namen sidebar.html gefunden")
+        console.error("Keine Datei namens sidebar.html gefunden");
     }
     const data = await response.text();
     document.getElementById("sidebar").innerHTML = data;
+
+    // Aktive Klasse für Sidebar-Buttons setzen
+    const currentPage = window.location.pathname.split("/").pop();
+    const buttons = document.querySelectorAll('.sidebar-button');
+    buttons.forEach(button => {
+      const link = button.getAttribute('href').split('/').pop();
+      if (currentPage === link) {
+        button.classList.add('active');
+      }
+    });
+
+    // Aktive Klasse für Footer-Links setzen
+    const footerLinks = document.querySelectorAll('.footer-text');
+    footerLinks.forEach(link => {
+      const linkPage = link.getAttribute('href').split('/').pop();
+      if (currentPage === linkPage) {
+        link.classList.add('active');
+      }
+    });
+    
   } catch (error) {
-    console.error("Error in load_sidebar: ", error)
+    console.error("Error in load_sidebar: ", error);
   }
 }
 loadSidebar();
+
+
