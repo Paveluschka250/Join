3fb167e9fc43f54ca7c43159ef8fa45a44fd6de1
yesser ben amor <yesserbenamor@yahoo.fr>
2024-10-13 19:15:01 +0200

    logic reg added

---
+++ js/reg.js
@@ -0,0 +1,53 @@
+
+
+let users = [
+    {
+     'email':'yesser@zf.com',
+      'password':'test123'
+    }
+]
+function addUser(){
+  let username = document.getElementById('benutzername').value;
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  let confirmedPw = document.getElementById('passwordConfirm').value;
+  users.push({email: email, password: password, username: username});
+ // print(users);
+
+  if(confirmedPw ===  password && password.length > 4){
+    sentDataToDB("/user", {"username":username, "email":email, "password":password});
+
+  }
+  else {
+    alert('pw');
+  }
+
+}
+//console.log(users);
+const _URL = "https://yesserdb-a0a02-default-rtdb.europe-west1.firebasedatabase.app";
+
+  // Registrierungsdaten werden zur DB gesendet
+  async function sentDataToDB(path="", data={}) {
+
+    try {
+      const response = await fetch( _URL + path + ".json", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+         body:JSON.stringify(data)
+      });
+      if (!response.ok) {
+        throw new Error('Fehler beim Schreiben der Benutzerdaten');
+      }
+    window.location.href = 'index.html?msg=Du hast dich erfolgreich registriert';
+
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Error registering user: ' + error.message);
+    }
+  }
+
+
+
+  
