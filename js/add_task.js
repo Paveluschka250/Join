af68f566b12df49e51be0d9452879e558ef299fc
ElStephano <165284686+ElStephano@users.noreply.github.com>
2024-12-29 11:59:15 +0100

    Subtasks delete-btn

--- js/add_task.js
+++ js/add_task.js
@@ -76,9 +76,20 @@ function addSubTask() {
     let subTask = document.getElementById('subtasks');
     let contentDiv = document.getElementById('subtask-content');
 
-    contentDiv.innerHTML += `<li><p>${subTask.value}<p/></li>`;
+    contentDiv.innerHTML += `
+        <li>
+            <div onclick="deleteSubTask(this)" class="li-elemente-subtask li-elements-overlayTask">
+                <p>${subTask.value}<p/>
+                <button><img src="../assets/icons/delete.svg"></img></button>    
+            </div>
+        </li>
+    `;
     subTask.value = '';
     closeNewSubtasksBtn()
+}
+
+function deleteSubTask(liElement) {
+  liElement.parentElement.remove();
 }
 
 function getTaskData(event) {
